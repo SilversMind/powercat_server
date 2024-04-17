8c022b71a9723f03845209dfd86351c1f7d33553
SilversMind <silversmind@gmail.com>
2024-04-17 23:17:29 +0200

    Update settings for DB connection

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,5 @@ WORKDIR /Users/bouzdi/Dev/powercat_server
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY src/ src/
-ENV NODE_OPTIONS="--max-old-space-size=8192"
-EXPOSE 3001
+EXPOSE 8080
 CMD ["python", "-m", "uvicorn", "src.main:app", "--reload", "--host", "0.0.0.0", "--port", "8080"]
--- src/settings.py
+++ src/settings.py
@@ -2,7 +2,6 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
 MONGODB_PASSWD = os.getenv("MONGODB_PASSWD")
-DB_URI = f"mongodb+srv://powercat:{MONGODB_PASSWD}@cluster0.flwq2ws.mongodb.net/?retryWrites=true&w=majority"
+DB_URI = f"mongodb+srv://powercat_server:{MONGODB_PASSWD}@cluster0.flwq2ws.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DB_NAME = "powercat"
