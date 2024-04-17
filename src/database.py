e07f31a69de17f0656fa6c0e8d6555f58045720e
SilversMind <silversmind@gmail.com>
2024-04-17 23:40:25 +0200

    Add tls for DB connection

--- src/database.py
+++ src/database.py
@@ -1,7 +1,9 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+import certifi
 from src.settings import DB_URI, DB_NAME
 
+ca = certifi.where()
 # Init MongoDB connection
-client = MongoClient(DB_URI, server_api=ServerApi('1'))
+client = MongoClient(DB_URI, server_api=ServerApi('1'), tlsCAFile=ca)
 DB = client[DB_NAME]
