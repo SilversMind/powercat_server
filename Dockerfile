fc137269938e66de115a78fe03910030f14fa208
SilversMind <silversmind@gmail.com>
2024-04-20 00:08:28 +0200

    Get back to same version of python as local

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.11
+FROM python:3.9
 WORKDIR /Users/bouzdi/Dev/powercat_server
 COPY requirements.txt .
 RUN pip install -r requirements.txt
--- src/database.py
+++ src/database.py
@@ -2,9 +2,7 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from src.settings import DB_URI, DB_NAME
 import certifi
-import logging
 
-logging.logging.info(certifi.where())
 # Init MongoDB connection
 client = MongoClient(DB_URI, server_api=ServerApi('1'), tlsCAFile=certifi.where())
 DB = client[DB_NAME]
