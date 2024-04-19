7731b14f50277c574c0b905ebfa482ecf6e0e1c6
SilversMind <silversmind@gmail.com>
2024-04-20 00:15:44 +0200

    Remove certificate

--- src/database.py
+++ src/database.py
@@ -4,5 +4,5 @@ from src.settings import DB_URI, DB_NAME
 import certifi
 
 # Init MongoDB connection
-client = MongoClient(DB_URI, server_api=ServerApi('1'), tlsCAFile=certifi.where())
+client = MongoClient(DB_URI, server_api=ServerApi('1'))
 DB = client[DB_NAME]
