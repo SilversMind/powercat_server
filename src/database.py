fe620ea59edf143ffd0715b18ee146f4b528d2b5
SilversMind <silversmind@gmail.com>
2024-04-19 15:40:09 +0200

    Changed MongoURI

--- src/database.py
+++ src/database.py
@@ -1,9 +1,7 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
-import certifi
 from src.settings import DB_URI, DB_NAME
 
-ca = certifi.where()
 # Init MongoDB connection
-client = MongoClient(DB_URI, server_api=ServerApi('1'), tlsCAFile=ca)
+client = MongoClient(DB_URI, server_api=ServerApi('1'))
 DB = client[DB_NAME]
--- src/settings.py
+++ src/settings.py
@@ -3,5 +3,5 @@ import os
 
 load_dotenv()
 MONGODB_PASSWD = os.getenv("MONGODB_PASSWD")
-DB_URI = f"mongodb+srv://powercat_server:{MONGODB_PASSWD}@cluster0.flwq2ws.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DB_URI = f"mongodb+srv://powercat_server:{MONGODB_PASSWD}@powercatdb.7m1t69i.mongodb.net/?retryWrites=true&w=majority&appName=PowercatDB"
 DB_NAME = "powercat"
