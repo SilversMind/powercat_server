c3f9654639eabcf609bb3d73891f6b37ee9153b8
SilversMind <silversmind@gmail.com>
2024-04-19 23:32:59 +0200

    try shit

--- src/database.py
+++ src/database.py
@@ -2,6 +2,8 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from src.settings import DB_URI, DB_NAME
 import certifi
+
+
 # Init MongoDB connection
-client = MongoClient(DB_URI, server_api=ServerApi('1'), tlsCAfile=certifi.where())
+client = MongoClient(DB_URI, server_api=ServerApi('1'), tlsCAFile=certifi.where())
 DB = client[DB_NAME]
