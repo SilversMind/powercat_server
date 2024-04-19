275266a23a7fa2dd64c45e361aa439a319442875
SilversMind <silversmind@gmail.com>
2024-04-19 23:54:01 +0200

    check certificate presence

--- src/database.py
+++ src/database.py
@@ -2,8 +2,9 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from src.settings import DB_URI, DB_NAME
 import certifi
+import logging
 
-
+logging.logging.info(certifi.where())
 # Init MongoDB connection
 client = MongoClient(DB_URI, server_api=ServerApi('1'), tlsCAFile=certifi.where())
 DB = client[DB_NAME]
