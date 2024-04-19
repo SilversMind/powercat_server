8628c1de07bfeaebc16ad36eac8b16e7779e54d2
SilversMind <silversmind@gmail.com>
2024-04-19 16:05:41 +0200

    Add certifi

--- src/database.py
+++ src/database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from src.settings import DB_URI, DB_NAME
-
+import certifi
 # Init MongoDB connection
-client = MongoClient(DB_URI, server_api=ServerApi('1'))
+client = MongoClient(DB_URI, server_api=ServerApi('1'), tlsCAfile=certifi.where())
 DB = client[DB_NAME]
